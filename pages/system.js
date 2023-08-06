78d25190928f2f2d20fdf43f40d708131fefc43d
diego francisco <diego444x@gmail.com>
2023-08-06 20:20:30 -0300

    sistema de agenda

--- pages/system.js
+++ pages/system.js
@@ -1,40 +1,56 @@
-const back = document.getElementById("back")
-const submit = document.getElementById("submit")
 
-function voltar(){
-    window.location.href = "../index.html"
+const res = document.getElementById('res');
+
+const arr = 
+[{
+  "nome": "João",
+  "horarios": [
+    "09:00",
+    "10:30",
+    "14:00",
+    "15:30"
+  ]
+},
+{
+  "nome": "Carlos",
+  "horarios": [
+    "10:00",
+    "11:30",
+    "13:00",
+    "16:30"
+  ]
+},
+{
+  "nome": "Miguel",
+  "horarios": [
+    "Dom 11:00",
+    "Seg 12:30",
+    "Quart 15:00",
+    "Sext 17:30"
+]
+}]
+
+function renderBarbeiros(barbeiros) {
+  barbeiros.forEach(barbeiro => {
+    const horariosHTML = barbeiro.horarios.map(horario => `<li><input type="checkbox" name="${barbeiro.nome}" id="">${horario}</li>`).join('');
+    res.innerHTML += `
+      <div class="container">
+        <div class="avatar">
+          <img src="../assets/logo.jpg" alt="">
+          <h2>${barbeiro.nome}</h2>
+        </div>
+        <p>Horarios disponiveis</p>
+        <ul>${horariosHTML}</ul>
+        <div class="redes">
+          <a href="#"><i class="bi bi-whatsapp"></i></a>
+          <a href="#"><i class="bi bi-facebook"></i></a>
+          <a href="#"><i class="bi bi-instagram"></i></a>
+        </div>
+      </div>
+    `;
+  });
 }
 
+// Chamada da função de renderização
+renderBarbeiros(arr);
 
-back.onclick = voltar
-
-  // Evento de envio do formulário
-  document.getElementById('meuFormulario').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio do formulário
-
-    // Obtém os valores dos campos do formulário
-    var nome = document.getElementById('nome').value;
-    var email = document.getElementById('email').value;
-    var telefone = document.getElementById('telefone').value;
-
-    // Obtém os valores dos horários selecionados
-    var horariosDisponiveis = [];
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-    checkboxes.forEach(function(checkbox) {
-      horariosDisponiveis.push(checkbox.value);
-    });
-
-    // Salva os valores em uma array ou executa a ação desejada
-    var dadosFormulario = {
-      nome: nome,
-      email: email,
-      telefone: telefone,
-      horarios: horariosDisponiveis
-    };
-    console.log(dadosFormulario);
-
-  });
-
-  nome.value = ""
-  email.value = ""
-  telefone.value = ""
